35ae3abcdd00ebe8c877f45f147aa8a22628aa8b
Mukhin001 <purgen.igor@yandex.ru>
2026-02-06 16:28:31 +0300

    Initial backend commit

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,11 @@ import { handleDevice } from "./handlers/handleDevice.js";
 import { handleUpdateSubscriptions } from "./handlers/user/handleUpdateSubscriptions.js";
 import { handleFeed } from "./handlers/user/handleFeed.js";
 // FRONTEND_URL = https://your-project.vercel.app
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3001";
+const FRONTEND_URL = (
+  process.env.FRONTEND_URL || "http://localhost:3000"
+).replace(/\/$/, "");
 const PORT = Number(process.env.PORT) || 3000;
-console.log(process.env.FRONTEND_URL);
+//console.log(process.env.FRONTEND_URL);
 
 const setCors = (res: ServerResponse) => {
   // ✅ Разрешаем запросы с других источников (например, фронтенда на 3000)
